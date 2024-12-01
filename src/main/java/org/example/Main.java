80bbc4aec450b7ab48c12e4f8261b5411378b734
luka4723 <lmilic4723rn@raf.rs>
2024-12-01 20:14:07 +0100

    lombok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.example.helper.PythonCommandExecutor;
 
 public class Main {
     public static void main(String[] args) {
-        String result = new PythonCommandExecutor().execute("import chess; b = chess.Board(); b.push(chess.Move(chess.B1, chess.C3)); print(b.fen())");
-        System.out.println(result);
+        System.out.println(new PythonCommandExecutor().execute("import chess;b=chess.Board();print(b.fen())"));
     }
 }
