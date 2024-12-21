0a9177e27371f04bd67662d13b61436c9b9a5966
luka4723 <lmilic4723rn@raf.rs>
2024-12-21 22:13:48 +0100

    minimax, pocetna heuristika

--- chess_engine/main.py
+++ chess_engine/main.py
@@ -45,12 +45,19 @@ def do_bot_move():
     fen = data.get('fen')
 
     board = chess.Board(fen)
+    if board.turn:
+        igrac = 1
+    else:
+        igrac = 0
 
     with chess.polyglot.open_reader('./Human.bin') as reader:
         try:
             board.push_san(reader.choice(board).move.uci())
         except IndexError:
-            board.push_san(list(board.legal_moves)[0].uci())
+            #print("123")
+            _, potez =min_max(board,float('-inf'),float('inf'),0,igrac)
+            #print(potez)
+            board.push(potez)
 
 
     return jsonify({'new_table': board.fen()})
@@ -70,6 +77,55 @@ def get_board_status():
         'winner': ("w" if board.outcome().winner == True else "b") if board.is_checkmate() else None
     })
 
+def min_max(board,alfa,beta,dubina,igrac):
+    if dubina==5 or board.is_game_over():
+        return heuristika(board), None
+
+    potez = None
+
+    if igrac==1:
+        m = float('-inf')
+        for move in board.legal_moves:
+            board.push(move)
+            temp, _ = min_max(board,alfa,beta,dubina+1,0)
+            board.pop()
+            if temp>m:
+                m=temp
+                potez = move
+            alfa = max(alfa,temp)
+            if beta<=alfa:
+                break
+        #print(dubina,potez,m)
+        return m, potez
+    else:
+        m = float('inf')
+        for move in board.legal_moves:
+            board.push(move)
+            temp,_ = min_max(board,alfa,beta,dubina+1,1)
+            board.pop()
+            if temp<m:
+                m=temp
+                potez = move
+            beta = min(beta,temp,m)
+            if beta<=alfa:
+                break
+        return m, potez
+
+def heuristika(board):
+
+    vr ={
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9,
+        chess.KING: 0
+    }
+    value = 0
+    for piece_type in vr:
+        value += len(board.pieces(piece_type, chess.WHITE)) * vr[piece_type]
+        value -= len(board.pieces(piece_type, chess.BLACK)) * vr[piece_type]
+    return value
 
 if __name__ == '__main__':
     app.run(debug=True)
